ca977d816e1cf7d20d09df6393628786a2db3849
wanghong <847773606@qq.com>
2022-05-05 09:19:14 +0800

    提交

---
+++ vue-music-hong/src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Recommend from '@/views/recommend'
+import Singer from '@/views/singer'
+import TopList from '@/views/top-list'
+import Search from '@/views/search'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/recommend'
+  },
+  {
+    path: '/recommend',
+    component: Recommend
+  },
+  {
+    path: '/singer',
+    component: Singer
+  },
+  {
+    path: '/top-list',
+    component: TopList
+  },
+  {
+    path: '/search',
+    component: Search
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
